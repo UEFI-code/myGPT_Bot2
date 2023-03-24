a567fcc9d4517c291cf5c82b04015057abb4c5a1
UEFI-code <1092700291@qq.com>
2023-03-24 00:37:42 +0000

    Add Subtitle

--- GPT_With_GUI_Bot.py
+++ GPT_With_GUI_Bot.py
@@ -4,7 +4,7 @@ import cv2
 myGPT3 = GPT3_Core.theGPT3(open('azgpt3.key','r').readline(), 2048)
 #myGPT3.ask('Hello World!')
 
-def show_simliar_figure(description):
+def show_simliar_figure(description, txtoutput):
     allfigures = os.listdir('figs')
     maxsimliar = 0
     selectedfigure = None
@@ -17,7 +17,10 @@ def show_simliar_figure(description):
             maxsimliar = simliar
             selectedfigure = i
     print('Selected Figure: ' + selectedfigure)
-    cv2.imshow('Bot', cv2.imread('figs/' + selectedfigure))
+    img = cv2.imread('figs/' + selectedfigure)
+    txtPos = (int(img.shape[0] * 0.1), int(img.shape[1] * 0.8))
+    cv2.putText(img, txtoutput, txtPos, cv2.FONT_HERSHEY_SIMPLEX, 0.8, (128, 128, 255), 2)
+    cv2.imshow('Bot', img)
     # make sure the window is on top
     cv2.setWindowProperty('Bot', cv2.WND_PROP_TOPMOST, 1)
     cv2.waitKey(1)
@@ -32,5 +35,5 @@ while True:
     print('Emotional: ' + Emotional)
     print('Action: ' + Action)
     #print('Founding Figure: ' + FigFile)
-    show_simliar_figure(DesiredFigFile)
+    show_simliar_figure(DesiredFigFile, TxtOutput)
     print('TxtOutput: ' + TxtOutput)
