82d614742c431565627856056e68a99efe5e2634
UEFI-code <1092700291@qq.com>
2023-04-15 06:57:08 +0800

    Add GPT4 Support

--- myGPT_Drv.py
+++ myGPT_Drv.py
@@ -47,9 +47,36 @@ class chat_Drv:
         response = requests.post(self.endpoint, headers=self.header, data=json.dumps(self.body))
         return response.json()['choices'][0]['message']['content']
 
+class GPT4_Drv:
+    def __init__(self, endpoint="https://mygpt233.openai.azure.com/openai/deployments/myGPT4_32K/chat/completions?api-version=2023-03-15-preview", apiKey="233333"):
+        self.endpoint = endpoint
+        self.header = {
+            "Content-Type": "application/json",
+            "api-key": apiKey
+        }
+        self.messages = [{"role":"system","content":"You are an AI assistant that helps people find information."}]
+        self.body = {
+            "messages": None,
+            "max_tokens": 2048,
+            "temperature": 0.7,
+            "top_p": 1,
+            "frequency_penalty": 0,
+            "presence_penalty": 0,
+            "stop": None
+        }
+    
+    def forward(self, x = 'Hello World'):
+        self.body['max_tokens'] = 32768 - len(x)
+        self.body['messages'] = self.messages + [{"role":"user","content":x}]
+        response = requests.post(self.endpoint, headers=self.header, data=json.dumps(self.body))
+        try:
+            return response.json()['choices'][0]['message']['content']
+        except:
+            return 'Net Error ' + str(response)
+    
 if __name__ == '__main__':
     #gpt3 = GPT3_Drv(apiKey=open('azgpt3.key', 'r').readline())
     #print(gpt3.forward('Hello World'))
-    chatgpt = chat_Drv(apiKey=open('azgpt3.key', 'r').readline())
+    chatgpt = GPT4_Drv(apiKey=open('azgpt3.key', 'r').readline())
     print(chatgpt.forward('Hello World'))
         
