9631b387f2b2f77f81a84795012be9e721e6cd7f
UEFI-code <1092700291@qq.com>
2023-05-08 14:22:49 +0800

    Bug Fix

--- myGPT_Drv.py
+++ myGPT_Drv.py
@@ -3,7 +3,7 @@ import requests
 import json
 
 class GPT3_Drv:
-    def __init__(self, maxTokens=3072, endpoint="https://mygpt233.openai.azure.com/openai/deployments/myGPT3/completions?api-version=2022-12-01", apiKey="233333"):
+    def __init__(self, endpoint="https://mygpt233.openai.azure.com/openai/deployments/myGPT3/completions?api-version=2022-12-01", apiKey="233333"):
         self.endpoint = endpoint
         self.header = {
             "Content-Type": "application/json",
@@ -11,7 +11,7 @@ class GPT3_Drv:
         }
         self.body = {
             "prompt": "Hello World",
-            "max_tokens": maxTokens,
+            "max_tokens": 512,
             "temperature": 0.7,
             "top_p": 1,
             "frequency_penalty": 0,
@@ -21,11 +21,14 @@ class GPT3_Drv:
     
     def forward(self, x = 'Hello World'):
         self.body['prompt'] = x
+        self.body['max_tokens'] = 4000 - len(x)
+        if (self.body['max_tokens'] < 0):
+            raise Exception('The input text is too long.')
         response = requests.post(self.endpoint, headers=self.header, data=json.dumps(self.body))
         return response.json()['choices'][0]['text']
 
 class chat_Drv:
-    def __init__(self, maxTokens=800, endpoint="https://mygpt233.openai.azure.com/openai/deployments/myChatGPT/chat/completions?api-version=2023-03-15-preview", apiKey="233333"):
+    def __init__(self, endpoint="https://mygpt233.openai.azure.com/openai/deployments/myChatGPT/chat/completions?api-version=2023-03-15-preview", apiKey="233333"):
         self.endpoint = endpoint
         self.header = {
             "Content-Type": "application/json",
@@ -34,7 +37,7 @@ class chat_Drv:
         self.messages = [{"role":"system","content":"You are a smart robot girl."}]
         self.body = {
             "messages": None,
-            "max_tokens": maxTokens,
+            "max_tokens": 256,
             "temperature": 0.7,
             "top_p": 1,
             "frequency_penalty": 0,
@@ -44,6 +47,9 @@ class chat_Drv:
     
     def forward(self, x = 'Hello World'):
         self.body['messages'] = self.messages + [{"role":"user","content":x}]
+        self.body['max_tokens'] = 3900 - len(x)
+        if (self.body['max_tokens'] < 0):
+            raise Exception('The input text is too long.')
         response = requests.post(self.endpoint, headers=self.header, data=json.dumps(self.body))
         return response.json()['choices'][0]['message']['content']
 
@@ -66,8 +72,10 @@ class GPT4_Drv:
         }
     
     def forward(self, x = 'Hello World'):
-        self.body['max_tokens'] = 32768 - len(x)
         self.body['messages'] = self.messages + [{"role":"user","content":x}]
+        self.body['max_tokens'] = 32700 - len(x)
+        if (self.body['max_tokens'] < 0):
+            raise Exception('The input text is too long.')
         response = requests.post(self.endpoint, headers=self.header, data=json.dumps(self.body))
         try:
             return response.json()['choices'][0]['message']['content']
