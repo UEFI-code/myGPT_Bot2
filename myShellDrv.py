145398d927f6405524255212a4ad3dad65c9fe6c
UEFI-code <zhuoshizhang@hotmail.com>
2024-08-09 15:29:09 +0800

    Improve Shell Driver

--- myShellDrv.py
+++ myShellDrv.py
@@ -3,10 +3,12 @@ import pty
 import select
 import fcntl
 import re
+import time
 
 class myShell():
-    def __init__(self, maxChars = 512):
+    def __init__(self, maxChars = 512, monitor = None):
         self.maxChars = maxChars
+        self.monitor = open(monitor, 'wb', buffering=0) if monitor != None else None
         self.ansi_escape = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~]')
         self.ptyData = b''
         self.os = os.name
@@ -22,20 +24,30 @@ class myShell():
             fcntl.fcntl(self.fd, fcntl.F_SETFL, flags | os.O_NONBLOCK)
     
     def getScreen(self):
+        delta = b''
         while True:
             r, _, _ = select.select([self.fd], [], [], 1.0)
             if r:
                 try:
-                    self.ptyData += os.read(self.fd, 2048)
+                    delta += os.read(self.fd, 2048)
                 except:
                     continue
             else:
                 # No data
                 break
+        self.ptyData += delta
+        
+        if self.monitor != None:
+            self.monitor.write(delta)
+        
+        decoded_delta = delta.decode('utf-8').replace('\r', '')
+        return decoded_delta[-self.maxChars:]
     
     def sendCmd(self, cmd):
         cmd += '\n'
         os.write(self.fd, cmd.encode('utf-8'))
+        time.sleep(0.1)
+        os.read(self.fd, len(cmd.encode('utf-8')))
     
     def translateScreen(self):
         translated = self.ansi_escape.sub('', self.ptyData.decode('utf-8')).replace('\r', '')
@@ -47,5 +59,5 @@ if __name__ == '__main__':
     myShellObj.getScreen()
     print(myShellObj.ptyData.decode('utf-8'))
     myShellObj.sendCmd('ifconfig')
-    myShellObj.getScreen()
+    print(myShellObj.getScreen())
     print(myShellObj.translateScreen())
